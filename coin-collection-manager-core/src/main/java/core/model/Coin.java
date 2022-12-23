e3d6bb3dc35c6f3738935546f490b15d78840d9c
Kevin Maggi <kevin.maggi@stud.unifi.it>
2022-12-24 00:35:51 +0100

    Created Coin class and year converter utility

---
+++ coin-collection-manager-core/src/main/java/core/model/Coin.java
@@ -0,0 +1,129 @@
+package core.model;
+
+import java.time.Year;
+import java.util.UUID;
+
+import org.hibernate.annotations.UuidGenerator;
+
+import core.utility.YearAttributeConverter;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+/**
+ * Represents a coin.
+ */
+@Entity
+@Table(name = "coins", uniqueConstraints = {
+		@UniqueConstraint(columnNames = {"grade", "country", "minting_year", "descriptio", "note"})})
+public class Coin {
+	
+	/**
+	 * Auto generated identifier.
+	 */
+	@Id
+	@GeneratedValue
+	@UuidGenerator
+	private UUID id;
+	
+	/**
+	 * Coin's conditions/quality.
+	 */
+	@Column(name = "grade")
+	private Grade grade;
+	
+	/**
+	 * Coin's country.
+	 */
+	@Column(name = "country")
+	private String country;
+	
+	/**
+	 * Coin's year of minting.
+	 */
+	@Column(name = "minting_year", columnDefinition = "int")
+	@Convert(converter = YearAttributeConverter.class)
+	private Year mintingYear;
+	
+	/**
+	 * Coin's description (e.g. "1$", "0.01£", "2€ commemorative: 30th anniversary of the Flag of Europe").
+	 */
+	@Column(name = "description")
+	private String description;
+	
+	/**
+	 * Possible notes (e.g. "minting error", "for exchange", ...).
+	 */
+	@Column(name = "note")
+	private String note;
+	
+	/**
+	 * Album where the coin is located
+	 */
+	@Column(name = "album")
+	private UUID album;
+
+	public Coin(Grade grade, String country, Year mintingYear, String description, String note, UUID album) {
+		super();
+		this.grade = grade;
+		this.country = country;
+		this.mintingYear = mintingYear;
+		this.description = description;
+		this.note = note;
+		this.album = album;
+	}
+
+	protected Coin() {}
+
+	public Grade getGrade() {
+		return grade;
+	}
+
+	public void setGrade(Grade grade) {
+		this.grade = grade;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Year getMintingYear() {
+		return mintingYear;
+	}
+
+	public void setMintingYear(Year mintingYear) {
+		this.mintingYear = mintingYear;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	public UUID getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(UUID album) {
+		this.album = album;
+	}
+}
---
+++ coin-collection-manager-core/src/test/java/core/utility/YearAttributeConverterTestCase.java
@@ -0,0 +1,56 @@
+package core.utility;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.time.Year;
+
+class YearAttributeConverterTestCase {
+	private static final int VALID_INT_YEAR = 1492;
+	private static final Year VALID_YEAR_YEAR = Year.of(1789);
+	
+	private YearAttributeConverter converter;
+	
+	@BeforeEach
+	public void setup() {
+		this.converter = new YearAttributeConverter();
+	}
+
+	@Nested
+	@DisplayName("Tests for the method that operate class-to-record conversion")
+	class ClassToRecord {
+		@Test
+		@DisplayName("Test that returns null when null year is passed")
+		public void testConvertToDatabaseColumnWhenYearIsNull() {
+			assertThat(converter.convertToDatabaseColumn(null)).isNull();
+		}
+		
+		@Test
+		@DisplayName("Test that returns the correct short when a non null year is passed")
+		public void testConvertToDatabaseColumnWhenYearIsNotNull() {
+			Year year = Year.of(VALID_INT_YEAR);
+			assertThat(converter.convertToDatabaseColumn(year)).isEqualTo(VALID_INT_YEAR);
+		}
+	}
+	
+	@Nested
+	@DisplayName("Tests for the method that operate record-to-class conversion")
+	class RecordToClass {
+		@Test
+		@DisplayName("Test that returns null when null short is passed")
+		public void testConvertToEntityAttributeWhenShortIsNull() {
+			assertThat(converter.convertToEntityAttribute(null)).isNull();
+		}
+		
+		@Test
+		@DisplayName("Test that returns the correct year when a non null short is passed")
+		public void testConvertToEntityAttributeWhenShortIsNotNull() {
+			int year_int = VALID_YEAR_YEAR.getValue();
+			assertThat(converter.convertToEntityAttribute(year_int)).isEqualTo(VALID_YEAR_YEAR);
+		}
+	}
+}
