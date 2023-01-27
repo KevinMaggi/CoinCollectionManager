c628306776d14e1bff4b67eeae8473737e4a6e4d
Kevin Maggi <kevin.maggi@stud.unifi.it>
2023-01-28 00:53:06 +0100

    ServiceIT for new service methods

--- coin-collection-manager-business/src/it/java/io/github/kevinmaggi/coin_collection_manager/business/service/transactional/TransactionalServiceWithPostgresIT.java
+++ coin-collection-manager-business/src/it/java/io/github/kevinmaggi/coin_collection_manager/business/service/transactional/TransactionalServiceWithPostgresIT.java
@@ -31,9 +31,10 @@ import jakarta.persistence.Persistence;
 
 class TransactionalServiceWithPostgresIT {
 	// Test variables
-	private Album ALBUM_PRE, ALBUM_COMM;
+	private Album ALBUM_PRE, ALBUM_COMM_1, ALBUM_COMM_2;
 	private Coin COIN_COMM_1, COIN_COMM_2, COIN_PRE;
 	private UUID INVALID_UUID = UUID.fromString("550e8400-e29b-41d4-a716-446655440000");
+	private static final String NEW_LOCATION = "new location";
 	
 	// Tests
 	private static EntityManagerFactory emf;
@@ -67,13 +68,13 @@ class TransactionalServiceWithPostgresIT {
 		void testTheInsertionOfAnAlbumWithSuccess() {
 			initAlbums();
 			
-			albumManager.addAlbum(ALBUM_COMM);
+			albumManager.addAlbum(ALBUM_COMM_1);
 			
 			em.getTransaction().begin();
-			Album fromDB = em.find(Album.class, ALBUM_COMM.getId());
+			Album fromDB = em.find(Album.class, ALBUM_COMM_1.getId());
 			em.getTransaction().commit();
 			
-			assertThat(fromDB).isEqualTo(ALBUM_COMM);
+			assertThat(fromDB).isEqualTo(ALBUM_COMM_1);
 		}
 		
 		@Test
@@ -81,7 +82,7 @@ class TransactionalServiceWithPostgresIT {
 		void testTheInsertionOfADuplicatedAlbum() {
 			populateDB();
 			
-			Album duplicate = new Album(ALBUM_COMM.getName(), ALBUM_COMM.getVolume(), "...", 0, 0);
+			Album duplicate = new Album(ALBUM_COMM_1.getName(), ALBUM_COMM_1.getVolume(), "...", 0, 0);
 			
 			assertThatThrownBy(() -> albumManager.addAlbum(duplicate))
 				.isInstanceOf(DuplicateAlbumException.class);
@@ -90,32 +91,32 @@ class TransactionalServiceWithPostgresIT {
 			List<Album> fromDB = em.createQuery("SELECT a FROM Album a", Album.class).getResultList();
 			em.getTransaction().commit();
 			
-			assertThat(fromDB).containsOnly(ALBUM_COMM, ALBUM_PRE);
+			assertThat(fromDB).containsOnly(ALBUM_COMM_1, ALBUM_COMM_2, ALBUM_PRE);
 		}
 		
 		@Test
-		@DisplayName("Test the update of an album with success")
-		void testTheUpdateOfAnAlbumWithSuccess() {
+		@DisplayName("Test the movement of an album with success")
+		void testTheMovementOfAnAlbumWithSuccess() {
 			populateDB();
-			ALBUM_COMM.setLocation("cassaforte");
 			
-			albumManager.updateAlbum(ALBUM_COMM);
+			Album returned = albumManager.moveAlbum(ALBUM_COMM_1, NEW_LOCATION);
 			
 			em.getTransaction().begin();
-			Album fromDB = em.find(Album.class, ALBUM_COMM.getId());
+			Album fromDB = em.find(Album.class, ALBUM_COMM_1.getId());
 			List<Album> listFromDB = em.createQuery("SELECT a FROM Album a", Album.class).getResultList();
 			em.getTransaction().commit();
 			
-			assertThat(fromDB).isEqualTo(ALBUM_COMM);
-			assertThat(listFromDB).containsOnly(ALBUM_COMM, ALBUM_PRE);
+			assertThat(returned.getLocation()).isEqualTo(NEW_LOCATION);
+			assertThat(fromDB).isEqualTo(ALBUM_COMM_1);
+			assertThat(listFromDB).containsOnly(ALBUM_COMM_1, ALBUM_COMM_2, ALBUM_PRE);
 		}
 		
 		@Test
-		@DisplayName("Test the update of a non-existing album")
-		void testTheUpdateOfANonExistingAlbum() {
+		@DisplayName("Test the movement of a non-existing album")
+		void testTheMovementOfANonExistingAlbum() {
 			initAlbums();
 			
-			assertThatThrownBy(() -> albumManager.updateAlbum(ALBUM_COMM))
+			assertThatThrownBy(() -> albumManager.moveAlbum(ALBUM_COMM_1, NEW_LOCATION))
 				.isInstanceOf(AlbumNotFoundException.class);
 			
 			em.getTransaction().begin();
@@ -130,14 +131,14 @@ class TransactionalServiceWithPostgresIT {
 		void testTheDeletionOfAnAlbumWithSuccess() {
 			populateDB();
 			
-			albumManager.deleteAlbum(ALBUM_COMM);
+			albumManager.deleteAlbum(ALBUM_COMM_1);
 			
 			em.getTransaction().begin();
 			List<Album> fromDB = em.createQuery("SELECT a FROM Album a", Album.class).getResultList();
 			List<Coin> coins = em.createQuery("SELECT c FROM Coin c", Coin.class).getResultList();
 			em.getTransaction().commit();
 			
-			assertThat(fromDB).containsOnly(ALBUM_PRE);
+			assertThat(fromDB).containsOnly(ALBUM_COMM_2, ALBUM_PRE);
 			assertThat(coins).containsOnly(COIN_PRE);
 		}
 		
@@ -146,7 +147,7 @@ class TransactionalServiceWithPostgresIT {
 		void testTheDeletionOfANonExistingAlbum() {
 			initAlbums();
 			
-			assertThatThrownBy(() -> albumManager.deleteAlbum(ALBUM_COMM))
+			assertThatThrownBy(() -> albumManager.deleteAlbum(ALBUM_COMM_1))
 				.isInstanceOf(AlbumNotFoundException.class);
 			
 			em.getTransaction().begin();
@@ -171,7 +172,7 @@ class TransactionalServiceWithPostgresIT {
 			
 			List<Album> retrieved = albumManager.findAllAlbums();
 			
-			assertThat(retrieved).containsOnly(ALBUM_COMM, ALBUM_PRE);
+			assertThat(retrieved).containsOnly(ALBUM_COMM_1, ALBUM_COMM_2, ALBUM_PRE);
 		}
 		
 		@Test
@@ -179,9 +180,9 @@ class TransactionalServiceWithPostgresIT {
 		void testTheRetrievalOfAnAlbumByIdInCaseOfSuccess() {
 			populateDB();
 			
-			Album retrieved = albumManager.findAlbumById(ALBUM_COMM.getId());
+			Album retrieved = albumManager.findAlbumById(ALBUM_COMM_1.getId());
 			
-			assertThat(retrieved).isEqualTo(ALBUM_COMM);
+			assertThat(retrieved).isEqualTo(ALBUM_COMM_1);
 		}
 		
 		@Test
@@ -195,9 +196,9 @@ class TransactionalServiceWithPostgresIT {
 		void testTheRetrievalOfAnAlbumByNameVolumeInCaseOfSuccess() {
 			populateDB();
 			
-			Album retrieved = albumManager.findAlbumByNameAndVolume(ALBUM_COMM.getName(), ALBUM_COMM.getVolume());
+			Album retrieved = albumManager.findAlbumByNameAndVolume(ALBUM_COMM_1.getName(), ALBUM_COMM_1.getVolume());
 			
-			assertThat(retrieved).isEqualTo(ALBUM_COMM);
+			assertThat(retrieved).isEqualTo(ALBUM_COMM_1);
 		}
 		
 		@Test
@@ -205,8 +206,8 @@ class TransactionalServiceWithPostgresIT {
 		void testTheRetrievalOfAnAlbumByNameVolumeInCaseOfUnsuccess() {
 			initAlbums();
 			
-			String name = ALBUM_COMM.getName();
-			int volume = ALBUM_COMM.getVolume();
+			String name = ALBUM_COMM_1.getName();
+			int volume = ALBUM_COMM_1.getVolume();
 			
 			assertThat(albumManager.findAlbumByNameAndVolume(name, volume)).isNull();
 		}
@@ -222,13 +223,13 @@ class TransactionalServiceWithPostgresIT {
 			persistAlbums();
 			initCoins();
 			
-			int occupiedSlot = ALBUM_COMM.getOccupiedSlots();
+			int occupiedSlot = ALBUM_COMM_1.getOccupiedSlots();
 			
 			coinManager.addCoin(COIN_COMM_1);
 						
 			em.getTransaction().begin();
 			Coin fromDB = em.find(Coin.class, COIN_COMM_1.getId());
-			Album album = em.find(Album.class, ALBUM_COMM.getId());
+			Album album = em.find(Album.class, ALBUM_COMM_1.getId());
 			em.getTransaction().commit();
 			
 			assertThat(fromDB).isEqualTo(COIN_COMM_1);
@@ -260,7 +261,7 @@ class TransactionalServiceWithPostgresIT {
 			persistAlbums();
 			initCoins();
 			
-			ALBUM_COMM.setOccupiedSlots(ALBUM_COMM.getNumberOfSlots());
+			ALBUM_COMM_1.setOccupiedSlots(ALBUM_COMM_1.getNumberOfSlots());
 			
 			assertThatThrownBy(() -> coinManager.addCoin(COIN_COMM_1))
 			.isInstanceOf(FullAlbumException.class);
@@ -273,31 +274,32 @@ class TransactionalServiceWithPostgresIT {
 		}
 		
 		@Test
-		@DisplayName("Test the update of a coin with success")
-		void testTheUpdateOfACoinWithSuccess() {
+		@DisplayName("Test the movement of a coin with success")
+		void testTheMovementOfACoinWithSuccess() {
 			populateDB();
-			
-			COIN_COMM_1.setNote("Errore di conio: asse ruotato 15°");
-			
-			coinManager.updateCoin(COIN_COMM_1);
+						
+			Coin returned = coinManager.moveCoin(COIN_COMM_1, ALBUM_COMM_2.getId());
 			
 			em.getTransaction().begin();
 			Coin fromDB = em.find(Coin.class, COIN_COMM_1.getId());
 			List<Coin> listFromDB = em.createQuery("SELECT c FROM Coin c", Coin.class).getResultList();
 			em.getTransaction().commit();
 			
+			assertThat(returned.getAlbum()).isEqualTo(ALBUM_COMM_2.getId());
 			assertThat(fromDB).isEqualTo(COIN_COMM_1);
 			assertThat(listFromDB).containsOnly(COIN_COMM_1, COIN_COMM_2, COIN_PRE);
 		}
 		
 		@Test
-		@DisplayName("Test the update of a non-existing coin")
-		void testTheUpdateOfANonExistingCoin() {
+		@DisplayName("Test the movement of a non-existing coin")
+		void testTheMovementOfANonExistingCoin() {
 			initAlbums();
 			persistAlbums();
 			initCoins();
 			
-			assertThatThrownBy(() -> coinManager.updateCoin(COIN_COMM_1))
+			UUID newAlbum = ALBUM_COMM_2.getId();
+			
+			assertThatThrownBy(() -> coinManager.moveCoin(COIN_COMM_1, newAlbum))
 				.isInstanceOf(CoinNotFoundException.class);
 			
 			em.getTransaction().begin();
@@ -308,18 +310,18 @@ class TransactionalServiceWithPostgresIT {
 		}
 		
 		@Test
-		@DisplayName("Test the move of a coin into a full album")
-		void testTheMoveOfACoinIntoAFullAlbum() {
+		@DisplayName("Test the movement of a coin into a full album")
+		void testTheMovementOfACoinIntoAFullAlbum() {
 			populateDB();
 						
-			ALBUM_COMM.setOccupiedSlots(ALBUM_COMM.getNumberOfSlots());
+			ALBUM_COMM_2.setOccupiedSlots(ALBUM_COMM_2.getNumberOfSlots());
 			em.getTransaction().begin();
-			em.merge(ALBUM_COMM);
+			em.merge(ALBUM_COMM_2);
 			em.getTransaction().commit();
 			
-			COIN_PRE.setAlbum(ALBUM_COMM.getId());
-						
-			assertThatThrownBy(() -> coinManager.updateCoin(COIN_PRE))
+			UUID newAlbum = ALBUM_COMM_2.getId();
+			
+			assertThatThrownBy(() -> coinManager.moveCoin(COIN_COMM_1, newAlbum))
 				.isInstanceOf(FullAlbumException.class);
 						
 			em.getTransaction().begin();
@@ -421,7 +423,7 @@ class TransactionalServiceWithPostgresIT {
 		void testTheRetrievalOfCoinsByAlbumInCaseOfSuccess() {
 			populateDB();
 			
-			List<Coin> retrieved = coinManager.findCoinsByAlbum(ALBUM_COMM.getId());
+			List<Coin> retrieved = coinManager.findCoinsByAlbum(ALBUM_COMM_1.getId());
 			
 			assertThat(retrieved).containsOnly(COIN_COMM_1, COIN_COMM_2);
 		}
@@ -471,20 +473,22 @@ class TransactionalServiceWithPostgresIT {
 	}
 
 	private void initCoins() {
-		COIN_COMM_1 = new Coin(Grade.AG, "Italy", Year.of(2004), "2€ comm. World Food Programme", "", ALBUM_COMM.getId());
-		COIN_COMM_2 = new Coin(Grade.AG, "Greece", Year.of(2004), "2€ comm. Olympics Game of Athen 2004", "", ALBUM_COMM.getId());
+		COIN_COMM_1 = new Coin(Grade.AG, "Italy", Year.of(2004), "2€ comm. World Food Programme", "", ALBUM_COMM_1.getId());
+		COIN_COMM_2 = new Coin(Grade.AG, "Greece", Year.of(2004), "2€ comm. Olympics Game of Athen 2004", "", ALBUM_COMM_1.getId());
 		COIN_PRE = new Coin(Grade.AG, "Italy", Year.of(1995), "500 Lire", "", ALBUM_PRE.getId());
 	}
 
 	private void persistAlbums() {
 		em.getTransaction().begin();
-		em.persist(ALBUM_COMM);
+		em.persist(ALBUM_COMM_1);
+		em.persist(ALBUM_COMM_2);
 		em.persist(ALBUM_PRE);
 		em.getTransaction().commit();
 	}
 	
 	private void initAlbums() {
 		ALBUM_PRE = new Album("Europa pre-euro", 1, "Armadio", 50, 0);
-		ALBUM_COMM = new Album("Euro commemorativi", 2, "Armadio", 50, 0);
+		ALBUM_COMM_1 = new Album("Euro commemorativi", 1, "Armadio", 50, 0);
+		ALBUM_COMM_2 = new Album("Euro commemorativi", 2, "Armadio", 50, 0);
 	}
 }
