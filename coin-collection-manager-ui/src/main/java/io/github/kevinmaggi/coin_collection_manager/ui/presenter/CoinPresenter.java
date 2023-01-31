d3794b842e9f67c512bbf6f44e35ad768a2df7e7
Kevin Maggi <kevin.maggi@stud.unifi.it>
2023-01-31 17:54:37 +0100

    Implemented CoinPresenter

--- coin-collection-manager-ui/src/main/java/io/github/kevinmaggi/coin_collection_manager/ui/presenter/CoinPresenter.java
+++ coin-collection-manager-ui/src/main/java/io/github/kevinmaggi/coin_collection_manager/ui/presenter/CoinPresenter.java
@@ -1,7 +1,13 @@
 package io.github.kevinmaggi.coin_collection_manager.ui.presenter;
 
+import java.util.Collections;
+import java.util.List;
+
 import io.github.kevinmaggi.coin_collection_manager.business.service.CoinManager;
+import io.github.kevinmaggi.coin_collection_manager.business.service.exception.CoinNotFoundException;
 import io.github.kevinmaggi.coin_collection_manager.business.service.exception.DatabaseException;
+import io.github.kevinmaggi.coin_collection_manager.business.service.exception.DuplicateCoinException;
+import io.github.kevinmaggi.coin_collection_manager.business.service.exception.FullAlbumException;
 import io.github.kevinmaggi.coin_collection_manager.core.model.Album;
 import io.github.kevinmaggi.coin_collection_manager.core.model.Coin;
 import io.github.kevinmaggi.coin_collection_manager.ui.view.View;
@@ -44,14 +50,51 @@ public class CoinPresenter extends Presenter {
 	}
 	
 	public synchronized void addCoin(Coin coin) {
-		
+		List<Coin> actualCoins = Collections.emptyList();
+		try {
+			actualCoins = manager.findAllCoins();
+			Coin added = manager.addCoin(coin);
+			view.coinAdded(added);
+			view.showSuccess("Coin successfully added: " + added.toString());
+		} catch (DatabaseException e) {
+			view.showError(DB_RETRIEVE_ERR_MSG);
+		} catch (FullAlbumException e) {
+			view.showError("Impossible to add the coin to this album because it is full");
+		} catch (DuplicateCoinException e) {
+			view.showError("This coin already exists");
+			view.showAllCoins(actualCoins);
+		}
 	}
 	
 	public synchronized void deleteCoin(Coin coin) {
-		
+		List<Coin> actualCoins = Collections.emptyList();
+		try {
+			actualCoins = manager.findAllCoins();
+			manager.deleteCoin(coin);
+			view.coinDeleted(coin);
+			view.showSuccess("Coin successfully deleted: " + coin.toString());
+		} catch (DatabaseException e) {
+			view.showError(DB_RETRIEVE_ERR_MSG);
+		} catch (CoinNotFoundException e) {
+			view.showError("This coin doesn't exist");
+			view.showAllCoins(actualCoins);
+		}
 	}
 	
 	public synchronized void moveCoin(Coin coin, Album newAlbum) {
-		
+		List<Coin> actualCoins = Collections.emptyList();
+		try {
+			actualCoins = manager.findAllCoins();
+			Coin moved = manager.moveCoin(coin, newAlbum.getId());
+			view.coinMoved(moved);
+			view.showSuccess("Coin successfully moved: " + moved.toString());
+		} catch (DatabaseException e) {
+			view.showError(DB_RETRIEVE_ERR_MSG);
+		} catch (FullAlbumException e) {
+			view.showError("Impossible to move the coin to this album because it is full");
+		} catch (CoinNotFoundException e) {
+			view.showError("This coin doesn't exist");
+			view.showAllCoins(actualCoins);
+		}
 	}
 }
