fa1a841fdecfb0ddeea98cce00adff9a6e6b18b5
Kevin Maggi <kevin.maggi@stud.unifi.it>
2023-02-13 18:29:53 +0100

    Using VNC instead of XVFB in CI
    as suggested by AssertJ swing doc. With XVFB there are some flaky tests

---
+++ execute-on-vnc.sh
@@ -0,0 +1,36 @@
+# Recommended by: https://joel-costigliola.github.io/assertj/assertj-swing-running.html
+# Source: https://joel-costigliola.github.io/assertj/assertj-swing-running.html
+
+# Search an available display
+NEW_DISPLAY=42
+DONE="no"
+
+while [ "$DONE" == "no" ]
+do
+  out=$(xdpyinfo -display :${NEW_DISPLAY} 2>&1)
+  if [[ "$out" == name* ]] || [[ "$out" == Invalid* ]]
+  then
+    # command succeeded; or failed with access error;  display exists
+    (( NEW_DISPLAY+=1 ))
+  else
+    # display doesn't exist
+    DONE="yes"
+  fi
+done
+
+echo "Using first available display :${NEW_DISPLAY}"
+
+# Start VNC
+OLD_DISPLAY=${DISPLAY}
+vncserver ":${NEW_DISPLAY}" -localhost -geometry 1920x1080 -depth 16
+export DISPLAY=:${NEW_DISPLAY}
+
+# Exec command
+"$@"
+EXIT_CODE=$?
+
+# Stop VNC
+export DISPLAY=${OLD_DISPLAY}
+vncserver -kill ":${NEW_DISPLAY}"
+
+exit $EXIT_CODE
