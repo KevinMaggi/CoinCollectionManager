4c842f440169c710d91f90f4a87df5a9f61fd974
Kevin Maggi <kevin.maggi@stud.unifi.it>
2023-02-16 19:07:41 +0100

    Configured app module and fatjar

---
+++ coin-collection-manager-app/src/main/java/io/github/kevinmaggi/coin_collection_manager/app/App.java
@@ -0,0 +1,13 @@
+package io.github.kevinmaggi.coin_collection_manager.app;
+
+public class App {
+
+	public App() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ coin-collection-manager-app/src/main/java/io/github/kevinmaggi/coin_collection_manager/app/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package provides a main class for running app
+ */
+package io.github.kevinmaggi.coin_collection_manager.app;
