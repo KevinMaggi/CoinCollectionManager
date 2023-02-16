f44f299715e8ac63e62d5b21a92571af9572e854
Kevin Maggi <kevin.maggi@stud.unifi.it>
2023-02-16 20:12:46 +0100

    Added picocli dependency and setted up main skeleton

--- coin-collection-manager-app/src/main/java/io/github/kevinmaggi/coin_collection_manager/app/App.java
+++ coin-collection-manager-app/src/main/java/io/github/kevinmaggi/coin_collection_manager/app/App.java
@@ -1,13 +1,35 @@
 package io.github.kevinmaggi.coin_collection_manager.app;
 
-public class App {
+import java.awt.EventQueue;
+import java.util.concurrent.Callable;
 
-	public App() {
-		// TODO Auto-generated constructor stub
-	}
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(mixinStandardHelpOptions = true)
+public class App implements Callable<Void> {
+	
+	private static final Logger LOGGER = LogManager.getLogger(App.class);
 
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		LOGGER.info("Starting app");
+		new CommandLine(new App()).execute(args);
 	}
 
+	@Override
+	public Void call() {
+		EventQueue.invokeLater(() -> {
+			try {
+				
+			} catch (Exception e) {
+				LOGGER.error(() -> "Something went wrong");
+				LOGGER.debug(() -> String.format("Caught Exception: %s", ExceptionUtils.getStackTrace(e)));
+			}
+		});
+		return null;
+	}
 }
