1ec035da66c3c0901dbd4557446bae8cd82573ff
Kevin Maggi <kevin.maggi@stud.unifi.it>
2023-02-21 19:38:40 +0100

    E2E test

--- coin-collection-manager-app/src/e2e/java/io/github/kevinmaggi/coin_collection_manager/app/AppE2E.java
+++ coin-collection-manager-app/src/e2e/java/io/github/kevinmaggi/coin_collection_manager/app/AppE2E.java
@@ -2,19 +2,24 @@ package io.github.kevinmaggi.coin_collection_manager.app;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.swing.launcher.ApplicationLauncher.*;
+import static org.assertj.swing.timing.Pause.pause;
+import static org.assertj.swing.timing.Timeout.timeout;
 
 import java.time.Year;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import javax.swing.JFrame;
 
 import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.finder.WindowFinder;
 import org.assertj.swing.fixture.FrameFixture;
 import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.assertj.swing.timing.Condition;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -39,8 +44,18 @@ public class AppE2E extends AssertJSwingJUnitTestCase {
 	private final String COIN_1_DESCRIPTION = "2€ comm. World Food Programme";
 	private final String COIN_2_DESCRIPTION = "2€ comm. Olympics Game of Athen 2004";
 	private final String COIN_NOTE = "";
+	
+	// Utility
+	private final String ALBUM_INSERT = 
+			"INSERT INTO albums (location, name, number_of_slots, number_of_occupied_slots, volume, id) " +
+			"VALUES ('%s', '%s', %s, %s, %s, '%s')";
+	private final String COIN_INSERT = 
+			"INSERT INTO coins (album, country, description, grade, minting_year, note, id) " +
+			"VALUES ('%s', '%s', '%s', %s, %s, '%s', '%s')";
 
 	// Tests
+	private static final int TIMEOUT = 5000;
+	
 	private static EntityManagerFactory emf;
 	private static EntityManager em;
 	
@@ -105,6 +120,341 @@ public class AppE2E extends AssertJSwingJUnitTestCase {
 			));
 	}
 	
+	@Test @GUITest
+	public void testSearchAlbumFunction() {
+		window.textBox("albumSearchName").enterText(ALBUM_NAME);
+		window.textBox("albumSearchVolume").enterText("1");
+		window.button(JButtonMatcher.withText("Search")).click();
+		
+		pause(new Condition("List contain single element") {
+			@Override
+			public boolean test() {
+				return window.list("albumList").contents().length == 1;
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.list("albumList").contents()).containsOnly(ALBUM_NAME + " vol.1 [0/" + ALBUM_SLOTS + "] (" + ALBUM_LOCATION + ")");
+	}
+	
+	@Test @GUITest
+	public void testAllAlbumsFunction() {
+		insertAlbum("Cassaforte", "Pre-euro", 30, 1, "550e8400-e29b-41d4-a716-446655440005");
+		
+		window.button(JButtonMatcher.withText("All albums")).click();
+		
+		pause(new Condition("List contain three element") {
+			@Override
+			public boolean test() {
+				return window.list("albumList").contents().length == 3;
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.list("albumList").contents())
+			.anySatisfy(e -> assertThat(e).contains(
+					"Pre-euro vol.1 [0/30] (Cassaforte)"
+			));
+	}
+	
+	@Test @GUITest
+	public void testAlbumSelectionFunctionOnSuccess() {
+		window.list("albumList").selectItem(Pattern.compile("Euro commemorative vol.1 .*"));
+		
+		pause(new Condition("Label change value") {
+			@Override
+			public boolean test() {
+				return window.label("albumSelection").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("albumSelection").text()).contains("Euro commemorative volume 1");
+	}
+	
+	@Test @GUITest
+	public void testAlbumSelectionFunctionOnFail() {
+		deleteAlbum(ALBUM_NAME, 1);
+		window.list("albumList").selectItem(Pattern.compile("Euro commemorative vol.1 .*"));
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("doesn't exist");
+	}
+	
+	@Test @GUITest
+	public void testAlbumDeselectionFunction() {
+		window.list("albumList").selectItem(Pattern.compile("Euro commemorative vol.1 .*"));
+		pause(new Condition("Label change value") {
+			@Override
+			public boolean test() {
+				return window.label("albumSelection").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		window.list("albumList").clearSelection();
+		
+		pause(new Condition("Label empty") {
+			@Override
+			public boolean test() {
+				return window.label("albumSelection").text() == " ";
+			}
+		}, timeout(TIMEOUT));
+	}
+	
+	@Test @GUITest
+	public void testDeleteAlbumFunctionOnSuccess() {
+		window.list("albumList").selectItem(Pattern.compile("Euro commemorative vol.1 .*"));
+		window.button(JButtonMatcher.withText("Delete album")).click();
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("successfully deleted");
+	}
+	
+	@Test @GUITest
+	public void testDeleteAlbumFunctionOnFail() {
+		window.list("albumList").selectItem(Pattern.compile("Euro commemorative vol.1 .*"));
+		deleteAlbum(ALBUM_NAME, 1);
+		window.button(JButtonMatcher.withText("Delete album")).click();
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("doesn't exist");
+	}
+	
+	@Test @GUITest
+	public void testMoveAlbumFunctionOnSuccess() {
+		window.list("albumList").selectItem(Pattern.compile("Euro commemorative vol.1 .*"));
+		window.button(JButtonMatcher.withText("Move album")).click();
+		window.dialog().textBox().enterText("New location");
+		window.dialog().button(JButtonMatcher.withText("OK")).click();
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("successfully moved");
+	}
+	
+	@Test @GUITest
+	public void testMoveAlbumFunctionOnFail() {
+		window.list("albumList").selectItem(Pattern.compile("Euro commemorative vol.1 .*"));
+		deleteAlbum(ALBUM_NAME, 1);
+		window.button(JButtonMatcher.withText("Move album")).click();
+		window.dialog().textBox().enterText("New location");
+		window.dialog().button(JButtonMatcher.withText("OK")).click();
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("doesn't exist");
+	}
+	
+	@Test @GUITest
+	public void testAddAlbumFunctionOnSuccess() {
+		window.textBox("albumFormName").enterText("Pre-euro");
+		window.textBox("albumFormVolume").enterText("1");
+		window.textBox("albumFormLocation").enterText("Cassaforte");
+		window.textBox("albumFormSlots").enterText("30");
+		window.button(JButtonMatcher.withText("Save album")).click();
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("successfully added");
+	}
+	
+	@Test @GUITest
+	public void testAddAlbumFunctionOnFail() {
+		window.textBox("albumFormName").enterText(ALBUM_NAME);
+		window.textBox("albumFormVolume").enterText("1");
+		window.textBox("albumFormLocation").enterText(ALBUM_LOCATION);
+		window.textBox("albumFormSlots").enterText(String.valueOf(ALBUM_SLOTS));
+		window.button(JButtonMatcher.withText("Save album")).click();
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("already exists");
+	}
+	
+	@Test @GUITest
+	public void testFilterCoinFunction() {
+		window.textBox("coinFilterDescription").enterText(COIN_1_DESCRIPTION);
+		window.button(JButtonMatcher.withText("Filter")).click();
+		
+		pause(new Condition("List contain single element") {
+			@Override
+			public boolean test() {
+				return window.list("coinList").contents().length == 1;
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.list("coinList").contents()).containsOnly(
+				"[" + COIN_1_COUNTRY + " " + COIN_YEAR + "] {" + COIN_GRADE.toString() + "} " + COIN_1_DESCRIPTION + " (" + COIN_NOTE + ")"
+		);
+	}
+	
+	@Test @GUITest
+	public void testAllCoinsFunction() {
+		insertCoin("550e8400-e29b-41d4-a716-446655440008", "Italy", "500 L.", Grade.PO, "1975", "", "550e8400-e29b-41d4-a716-446655440009");
+		
+		window.button(JButtonMatcher.withText("All coins")).click();
+		
+		pause(new Condition("List contain three element") {
+			@Override
+			public boolean test() {
+				return window.list("coinList").contents().length == 3;
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.list("coinList").contents())
+			.anySatisfy(e -> assertThat(e).contains(
+					"[Italy 1975] {PO} 500 L. ()"
+			));
+	}
+	
+	@Test @GUITest
+	public void testCoinSelectionFunctionOnSuccess() {
+		window.list("coinList").selectItem(Pattern.compile(".*World Food Programme.*"));
+		
+		pause(new Condition("Label change value") {
+			@Override
+			public boolean test() {
+				return window.label("coinSelection").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("coinSelection").text()).contains("World Food Programme");
+	}
+	
+	@Test @GUITest
+	public void testCoinSelectionFunctionOnFail() {
+		deleteCoin(COIN_GRADE, COIN_1_COUNTRY, COIN_1_DESCRIPTION, COIN_NOTE, COIN_YEAR);
+		window.list("coinList").selectItem(Pattern.compile(".*World Food Programme.*"));
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("doesn't exist");
+	}
+	
+	@Test @GUITest
+	public void testCoinDeselectionFunction() {
+		window.list("coinList").selectItem(Pattern.compile(".*World Food Programme.*"));
+		pause(new Condition("Label change value") {
+			@Override
+			public boolean test() {
+				return window.label("coinSelection").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		window.list("coinList").clearSelection();
+		
+		pause(new Condition("Label empty") {
+			@Override
+			public boolean test() {
+				return window.label("coinSelection").text() == " ";
+			}
+		}, timeout(TIMEOUT));
+	}
+	
+	@Test @GUITest
+	public void testDeleteCoinFunctionOnSuccess() {
+		window.list("coinList").selectItem(Pattern.compile(".*World Food Programme.*"));
+		window.button(JButtonMatcher.withText("Delete coin")).click();
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("successfully deleted");
+	}
+	
+	@Test @GUITest
+	public void testDeleteCoinFunctionOnFail() {
+		window.list("coinList").selectItem(Pattern.compile(".*World Food Programme.*"));
+		deleteCoin(COIN_GRADE, COIN_1_COUNTRY, COIN_1_DESCRIPTION, COIN_NOTE, COIN_YEAR);
+		window.button(JButtonMatcher.withText("Delete coin")).click();
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("doesn't exist");
+	}
+	
+	@Test @GUITest
+	public void testMoveCoinFunctionOnSuccess() {
+		window.list("coinList").selectItem(Pattern.compile(".*World Food Programme.*"));
+		window.button(JButtonMatcher.withText("Move coin")).click();
+		window.dialog().comboBox().selectItem(Pattern.compile("Euro commemorative vol.2.*"));
+		window.dialog().button(JButtonMatcher.withText("OK")).click();
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("successfully moved");
+	}
+	
+	@Test @GUITest
+	public void testMoveCoinFunctionOnFail() {
+		window.list("coinList").selectItem(Pattern.compile(".*World Food Programme.*"));
+		deleteCoin(COIN_GRADE, COIN_1_COUNTRY, COIN_1_DESCRIPTION, COIN_NOTE, COIN_YEAR);
+		window.button(JButtonMatcher.withText("Move coin")).click();
+		window.dialog().comboBox().selectItem(Pattern.compile("Euro commemorative vol.2.*"));
+		window.dialog().button(JButtonMatcher.withText("OK")).click();
+		
+		pause(new Condition("Status change value") {
+			@Override
+			public boolean test() {
+				return window.label("status").text() != " ";
+			}
+		}, timeout(TIMEOUT));
+		
+		assertThat(window.label("status").text()).contains("doesn't exist");
+	}
+	
 	@Override
 	protected void onTearDown() throws Exception {
 		em.clear();
@@ -117,47 +467,67 @@ public class AppE2E extends AssertJSwingJUnitTestCase {
 	}
 
 	private void populateDB() {
-		String ALBUM_INSERT = 
-				"INSERT INTO albums (location, name, number_of_slots, number_of_occupied_slots, volume, id) " +
-				"VALUES ('%s', '%s', %s, %s, %s, '%s')";
-		String COIN_INSERT = 
-				"INSERT INTO coins (album, country, description, grade, minting_year, note, id) " +
-				"VALUES ('%s', '%s', '%s', %s, %s, '%s', '%s')";
-		
-		em.getTransaction().begin();
-		em.createNativeQuery(String.format(ALBUM_INSERT, 
+		insertAlbum(
 				ALBUM_LOCATION, 
 				ALBUM_NAME, 
 				ALBUM_SLOTS,
-				0, 
 				1, 
 				"550e8400-e29b-41d4-a716-446655440000"
-		)).executeUpdate();
-		em.createNativeQuery(String.format(ALBUM_INSERT, 
+		);
+		insertAlbum(
 				ALBUM_LOCATION, 
 				ALBUM_NAME, 
 				ALBUM_SLOTS, 
-				0, 
 				2, 
 				"550e8400-e29b-41d4-a716-446655440001"
-		)).executeUpdate();
+		);
 		
-		em.createNativeQuery(String.format(COIN_INSERT, 
+		insertCoin(
 				"550e8400-e29b-41d4-a716-446655440000", 
-				COIN_1_COUNTRY, COIN_1_DESCRIPTION, 
-				COIN_GRADE.ordinal(), 
-				Year.parse(COIN_YEAR), 
+				COIN_1_COUNTRY,
+				COIN_1_DESCRIPTION, 
+				COIN_GRADE, 
+				COIN_YEAR, 
 				COIN_NOTE, 
 				"550e8400-e29b-41d4-a716-446655440002"
-		)).executeUpdate();
-		em.createNativeQuery(String.format(COIN_INSERT, 
+		);
+		insertCoin(
 				"550e8400-e29b-41d4-a716-446655440000", 
-				COIN_2_COUNTRY, COIN_2_DESCRIPTION, 
-				COIN_GRADE.ordinal(), 
-				Year.parse(COIN_YEAR), 
+				COIN_2_COUNTRY,
+				COIN_2_DESCRIPTION, 
+				COIN_GRADE, 
+				COIN_YEAR, 
 				COIN_NOTE, 
 				"550e8400-e29b-41d4-a716-446655440003"
-		)).executeUpdate();
+		);
+	}
+	
+	private void insertAlbum(String location, String name, int slots, int volume, String id) {
+		em.getTransaction().begin();
+		em.createNativeQuery(String.format(ALBUM_INSERT, location, name, slots, 0, volume, id))
+				.executeUpdate();
+		em.getTransaction().commit();
+	}
+	
+	private void insertCoin(String album, String country, String description, Grade grade, String year, String note, String id) {
+		em.getTransaction().begin();
+		em.createNativeQuery(String.format(COIN_INSERT, album, country, description, grade.ordinal(), Year.parse(year), note, id))
+				.executeUpdate();
+		em.getTransaction().commit();
+	}
+	
+	private void deleteAlbum(String name, int volume) {
+		String ALBUM_DELETE = "DELETE FROM albums WHERE name='%s' AND volume=%d";
+		em.getTransaction().begin();
+		em.createNativeQuery(String.format(ALBUM_DELETE, name, volume)).executeUpdate();
+		em.getTransaction().commit();
+	}
+	
+	private void deleteCoin(Grade grade, String country, String description, String note, String year) {
+		String COIN_DELETE = "DELETE FROM coins WHERE grade=%d AND country='%s' AND description='%s' AND note='%s' AND minting_year=%d";
+		em.getTransaction().begin();
+		em.createNativeQuery(String.format(COIN_DELETE, grade.ordinal(), country, description, note, Integer.valueOf(year)))
+			.executeUpdate();
 		em.getTransaction().commit();
 	}
 }
